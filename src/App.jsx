1a29eb64c02e4059c492877604ca312d2049c63c
Nitish Sharma <ernitishsharma361@gmail.com>
2026-08-13 22:45:45 +0530

    feat: initialize React app with navigation and styling

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+      <div className="main">
+        <h1>2 AM</h1>
+        <h1>INDIA</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+        <h3>time</h3>
+        <ul>
+            <li>Playlists</li>
+            <li>Wallpaper</li>
+            <li>[ ]</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
